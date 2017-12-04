65cb7155a783528f8d6d3cb4d88d0d8eb382f771
SeanEstey <estese@gmail.com>
2017-12-04 01:48:05 -0700

    almost all indicators code converted to using pandas dataframes

--- app/main/indicators.py
+++ app/main/indicators.py
@@ -76,136 +76,63 @@ def build_series(ex, pair, start, end):
 
 #---------------------------------------------------------------
 def analyze_trades(ex, pair, start, end):
-
+    """Analyze recorded trades.
     """
-    # DATAFRAME CODE
     df = json_normalize(list(g.db['pub_trades'].find({
         'ex':ex, 'pair':pair, 'date':{'$gte':start, '$lte':end}
     })))
+
     df_buy = df.loc[ df['side'] == 'buy' ]
     df_sell = df.loc[ df['side'] == 'sell' ]
 
     return {
-        'n_buys': df_buy.count()['_id'],
-        'buy_vol': df_buy['volume'].sum(),
-        'n_sells': df_sell.count()['_id'],
-        'sell_vol': df_sell['volume'].sum(),
-        'price': df['price'].mean(),
-        'price_diff': df.loc[-1]['price'] - df.loc[0]['price'],
-        'buy_rate': results['n_buys'] / df.count()['_id'],
-        'vol_diff': results['buy_vol'] - results['sell_vol']
-    }
-    """
-
-    ind = {
-        'price':[],
-        'buy_vol':0.0,
-        'sell_vol':0.0,
-        'n_buys':0,
-        'n_sells':0,
-        'buy_rate':0
-    }
-    trades = g.db['pub_trades'].find({
-        'ex':ex, 'pair':pair, 'date':{'$gte':start, '$lt':end}
-    })
-    for t in trades:
-        ind['price'].append(t['price'])
-
-        if t.get('side','') == 'buy':
-            ind['buy_vol'] += t['volume']
-            ind['n_buys'] += 1
-        elif t.get('side','') == 'sell':
-            ind['sell_vol'] += t['volume']
-            ind['n_sells'] += 1
-
-    return {
-        'n_buys': ind['n_buys'],
-        'n_sells': ind['n_sells'],
-        'buy_vol': ind['buy_vol'],
-        'sell_vol': ind['sell_vol'],
-        # Avg price
-        'price': round(sum(ind['price'])/len(ind['price']),2) if len(ind['price']) > 0 else 0.0,
-        # Percent price change
-        'price_diff': round(((ind['price'][-1] - ind['price'][0]) / ind['price'][0]) * 100, 2),
-        # Num Buys/Total Trades ratio
-        'buy_rate': round(ind['n_buys']/(ind['n_buys']+ind['n_sells']),2) if ind['n_buys']+ind['n_sells'] > 0 else 0.0,
-        'vol_diff': ind['buy_vol'] - ind['sell_vol']
+        'n_buys': int(df_buy.count()['_id']),
+        'n_sells': int(df_sell.count()['_id']),
+        'buy_vol': round(float(df_buy['volume'].sum()), 5),
+        'sell_vol': round(float(df_sell['volume'].sum()), 5),
+        'vol_diff': round(float(df_buy['volume'].sum() - df_sell['volume'].sum()), 5),
+        'price': round(float(df['price'].mean()), 2),
+        'price_diff': round(float(df.iloc[-1]['price'] - df.iloc[0]['price']), 2),
+        'buy_rate': round(float(df_buy.count()['_id'] / df.count()['_id']), 5)
     }
 
 #---------------------------------------------------------------
 def analyze_ob(ex, pair, start=None, end=None):
     """Find indicators from examining structure of orderbooks.
     """
-
-    """
-    # DATAFRAME CODE
     df = json_normalize(list(g.db['pub_books'].find({
         'ex':ex, 'pair':pair, 'date':{'$gte':start, '$lte':end}
     })))
 
-    return {
-        'ask_price': df['asks'][0].mean(),
-        'bid_price': df['bids'][0].mean(),
-        'ask_vol': df['asks'][1].sum(),
-        'bid_vol': df['bids'][1].sum(),
-        'bid_inertia': ???
-        'ask_inertia': ???
-    }
-    """
+    df['ask_price'] = [ row[0][0] for row in df['asks'] ]
+    df['ask_vol'] = [ row[0][1] for row in df['asks'] ]
+    df['bid_price'] = [ row[0][0] for row in df['bids'] ]
+    df['bid_vol'] = [ row[0][1] for row in df['bids'] ]
 
-    if start is None and end is None:
-        docs = g.db['pub_books'].find({'ex':ex, 'pair':pair}).sort('date',-1).limit(1)
-    else:
-        docs = g.db['pub_books'].find({'ex':ex, 'pair':pair, 'date':{'$gte':start, '$lt':end}})
-
-    # For each snapshot in series, find av determine indicators
-    ask_prices = []
-    bid_prices = []
-    ask_vols = []
-    bid_vols = []
     ask_inertias = []
     bid_inertias = []
+    bid_cutoff_price = float(df['bid_price'][0] * 0.99)
+    ask_cutoff_price = float(df['ask_price'][0] * 1.01)
 
-    for doc in docs:
-        asks = doc['asks']
-        bids = doc['bids']
+    for index, row in df.iterrows():
+        rem_asks = [ n[1] for n in row['asks'] if n[0] >= ask_cutoff_price ]
+        ask_inertias.append(rem_asks[0]) if len(rem_asks) > 0 else False
 
-        # Price
-        bid_prices.append(float(bids[0][0]))
-        ask_prices.append(float(asks[0][0]))
+        rem_bids = [ n[1] for n in row['bids'] if n[0] >= bid_cutoff_price ]
+        bid_inertias.append(rem_bids[0]) if len(rem_bids) > 0 else False
 
-        # Bid/Ask volume sums
-        ask_vols.append( sum([float(n[1]) for n in asks]) )
-        bid_vols.append( sum([float(n[1]) for n in bids]) )
+    df['ask_inertias'] = ask_inertias
+    df['bid_inertias'] = bid_inertias
 
-        # Bid/Ask inertia: amount of order book volume needing to be executed
-        # to move bid/ask price >= 1%. Lower values may predict sudden price swings.
-        inertia = 0.0
-        for b in bids:
-            inertia += float(b[1])
-            if float(b[0]) <= float(bids[0][0]) * 0.99:
-                break
-        bid_inertias.append(inertia)
-        for a in asks:
-            inertia += float(a[1])
-            if float(a[0]) >= float(asks[0][0]) * 1.01:
-                break
-        ask_inertias.append(inertia)
-
-    # Take average of all indicators.
-    results = {
-        'bid_price': round(sum(bid_prices)/len(bid_prices),2) if len(bid_prices) > 0 else 0.0,
-        'ask_price': round(sum(ask_prices)/len(ask_prices),2) if len(ask_prices) > 0 else 0.0,
-        'bid_vol': round(sum(bid_vols)/len(bid_vols),5) if len(bid_vols) > 0 else 0.0,
-        'ask_vol': round(sum(ask_vols)/len(ask_vols),5) if len(ask_vols) > 0 else 0.0,
-        'bid_inertia': round(sum(bid_inertias)/len(bid_inertias), 2) if len(bid_inertias) > 0 else 0.0,
-        'ask_inertia': round(sum(ask_inertias)/len(ask_inertias), 2) if len(ask_inertias) > 0 else 0.0
+    return {
+        'mean_ask': round(float(df['ask_price'].mean()), 2),
+        'mean_bid': round(float(df['bid_price'].mean()), 2),
+        'sum_ask_vol': round(float(df['ask_vol'].sum()), 5),
+        'sum_bid_vol': round(float(df['bid_vol'].sum()), 5),
+        'vol_spread': round(float(df['bid_vol'].sum() - df['ask_vol'].sum()), 5),
+        'mean_ask_inertia': round(float(df['ask_inertias'].mean()), 5),
+        'mean_bid_inertia': round(float(df['bid_inertias'].mean()), 5),
     }
-
-    # Avg bid volume - Avg ask volume
-    results['vol_diff'] = results['bid_vol'] - results['ask_vol']
-
-    return results
 
 #---------------------------------------------------------------
 def analyze_ob_actions(ex, pair, start=None, end=None):
@@ -233,5 +160,4 @@ def analyze_ob_actions(ex, pair, start=None, end=None):
         'n_asks_executed': int(df2.loc[ df2['action'] == 'trade' ].count()['_id']),
         'sum_ask_events': int(df2.count()['_id'])
     })
-
     return results
--- db_shell.py
+++ db_shell.py
@@ -1,12 +1,32 @@
+from datetime import datetime, timedelta
 import pymongo
 import pandas as pd
 from pandas import DataFrame
 from pandas.io.json import json_normalize
-
+from dateutil.parser import parse
 client = pymongo.MongoClient()
 db = client['simbot']
+ex = 'QuadrigaCX'
+pair = ['btc','cad']
 
+####### analyze_ob ##########
+end = parse("2017-12-03T18:15:48.000Z")
+start = end + timedelta(minutes=-10)
+df = json_normalize(list(db['pub_trades'].find({
+    'ex':ex, 'pair':pair, 'date':{'$gte':start, '$lte':end}
+})))
 
-datapoints = list(db['chart_series'].find({}))
-df = json_normalize(datapoints)
+df_buy = df.loc[ df['side'] == 'buy' ]
+df_sell = df.loc[ df['side'] == 'sell' ]
 
+results = {
+    'n_buys': int(df_buy.count()['_id']),
+    'buy_vol': round(float(df_buy['volume'].sum()), 5),
+    'n_sells': int(df_sell.count()['_id']),
+    'sell_vol': round(float(df_sell['volume'].sum()), 5),
+    'price': round(float(df['price'].mean()), 2),
+    'price_diff': round(float(df.iloc[-1]['price'] - df.iloc[0]['price']), 2),
+}
+
+results['buy_rate'] = round(float(results['n_buys'] / df.count()['_id']), 5)
+results['vol_diff'] = round(float(results['buy_vol'] - results['sell_vol']), 5)
